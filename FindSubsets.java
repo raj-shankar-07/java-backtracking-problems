cbab876e20d6d59e95c116e470c02b833144e76c
raj-shankar-07 <rajshankar05082004@gmail.com>
2024-02-04 09:55:17 +0530

    Add Initial Commit

---
+++ FindSubsets.java
@@ -0,0 +1,24 @@
+package backtracking;
+
+public class FindSubsets {
+
+    public static void findSubsets(String str, String ans, int i) {
+        //Base case
+        if(i == str.length()) {
+            if(ans.length() == 0) {
+                System.out.println("null");
+            } else {
+                System.out.println(ans);
+            }
+            return;
+        }
+        //yes coice
+        findSubsets(str, ans+str.charAt(i), i+1);
+        //No choice
+        findSubsets(str, ans, i+1);
+    }
+    public static void main(String[] args) {
+        String str = "abc";
+        findSubsets(str, "", 0);
+    }
+}
